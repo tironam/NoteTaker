d196ce68b020abc679907961ee1fed24747fbd57
Michael Tirona <tironamichael@gmail.com>
2020-05-22 15:40:13 -0700

    Added code to server.js file

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 const express = require('express')
+const { join } = require('path')
 const app = express()
-const writeFileSync = require('fs')
+const readFile = require('fs')
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/notes', (req, res) => {
+    res.sendFile(join(__dirname, 'public', 'notes.html'))
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(3000, () => console.log('http://localhost:3000'))
