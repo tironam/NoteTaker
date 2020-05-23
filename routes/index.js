dad1b2da863f8665440832d12f1208ac9ab46f41
Michael Tirona <tironamichael@gmail.com>
2020-05-22 17:16:26 -0700

    Trying to figure it out

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router()
 const db = require('../db')
 
-router.get('')
+const notes = (require('../db/db.json'))
+
+router.get('/notes', (req, res) => {
+    res.json(notes)
+})
 
 router.use()
+
+module.exports = Index
